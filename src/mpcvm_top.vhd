d75029b25f4d93c37200ab44a55c025ed1223f5a
GLdL <guillermo.jose.loustau.de.linares@cern.ch>
2021-05-17 17:29:57 +0200

    commented XPM memory because failing at gitlab-ci

--- src/mpcvm_top.vhd
+++ src/mpcvm_top.vhd
@@ -569,86 +569,86 @@ begin
 
     end generate;
 
-    XPM: if g_MEMORY_STRUCTURE = "XPM" generate
+    -- XPM: if g_MEMORY_STRUCTURE = "XPM" generate
 
-      xpm_memory_sdpram_inst : xpm_memory_sdpram
-      generic map (
-         ADDR_WIDTH_A => ADD_WIDTH,               -- DECIMAL
-         ADDR_WIDTH_B => ADD_WIDTH,               -- DECIMAL
-         AUTO_SLEEP_TIME => 0,            -- DECIMAL
-         BYTE_WRITE_WIDTH_A => MEM_WIDTH,--32,        -- DECIMAL
-         CASCADE_HEIGHT => 0,             -- DECIMAL
-         CLOCKING_MODE => "common_clock", -- String
-         ECC_MODE => "no_ecc",            -- String
-         MEMORY_INIT_FILE => "none",      -- String
-         MEMORY_INIT_PARAM => "0",        -- String
-         MEMORY_OPTIMIZATION => "true",   -- String
-         MEMORY_PRIMITIVE => "ultra",      -- String
-         MEMORY_SIZE => MEM_WIDTH * MEM_DEPTH,             -- DECIMAL
-         MESSAGE_CONTROL => 0,            -- DECIMAL
-         READ_DATA_WIDTH_B => MEM_WIDTH,--32,         -- DECIMAL
-         READ_LATENCY_B => 2,             -- DECIMAL
-         READ_RESET_VALUE_B => "0",       -- String
-         RST_MODE_A => "SYNC",            -- String
-         RST_MODE_B => "SYNC",            -- String
-         SIM_ASSERT_CHK => 0,             -- DECIMAL; 0=disable simulation messages, 1=enable simulation messages
-         USE_EMBEDDED_CONSTRAINT => 0,    -- DECIMAL
-         USE_MEM_INIT => 1,               -- DECIMAL
-         WAKEUP_TIME => "disable_sleep",  -- String
-         WRITE_DATA_WIDTH_A => MEM_WIDTH,--32,        -- DECIMAL
-         WRITE_MODE_B => "read_first"      -- String
-      )
-      port map (
-        --  dbiterrb => dbiterrb,             -- 1-bit output: Status signal to indicate double bit error occurrence
-                                           -- on the data output of port B.
+    --   xpm_memory_sdpram_inst : xpm_memory_sdpram
+    --   generic map (
+    --      ADDR_WIDTH_A => ADD_WIDTH,               -- DECIMAL
+    --      ADDR_WIDTH_B => ADD_WIDTH,               -- DECIMAL
+    --      AUTO_SLEEP_TIME => 0,            -- DECIMAL
+    --      BYTE_WRITE_WIDTH_A => MEM_WIDTH,--32,        -- DECIMAL
+    --      CASCADE_HEIGHT => 0,             -- DECIMAL
+    --      CLOCKING_MODE => "common_clock", -- String
+    --      ECC_MODE => "no_ecc",            -- String
+    --      MEMORY_INIT_FILE => "none",      -- String
+    --      MEMORY_INIT_PARAM => "0",        -- String
+    --      MEMORY_OPTIMIZATION => "true",   -- String
+    --      MEMORY_PRIMITIVE => "ultra",      -- String
+    --      MEMORY_SIZE => MEM_WIDTH * MEM_DEPTH,             -- DECIMAL
+    --      MESSAGE_CONTROL => 0,            -- DECIMAL
+    --      READ_DATA_WIDTH_B => MEM_WIDTH,--32,         -- DECIMAL
+    --      READ_LATENCY_B => 2,             -- DECIMAL
+    --      READ_RESET_VALUE_B => "0",       -- String
+    --      RST_MODE_A => "SYNC",            -- String
+    --      RST_MODE_B => "SYNC",            -- String
+    --      SIM_ASSERT_CHK => 0,             -- DECIMAL; 0=disable simulation messages, 1=enable simulation messages
+    --      USE_EMBEDDED_CONSTRAINT => 0,    -- DECIMAL
+    --      USE_MEM_INIT => 1,               -- DECIMAL
+    --      WAKEUP_TIME => "disable_sleep",  -- String
+    --      WRITE_DATA_WIDTH_A => MEM_WIDTH,--32,        -- DECIMAL
+    --      WRITE_MODE_B => "read_first"      -- String
+    --   )
+    --   port map (
+    --     --  dbiterrb => dbiterrb,             -- 1-bit output: Status signal to indicate double bit error occurrence
+    --                                        -- on the data output of port B.
    
-         doutb => mem_out_b,--doutb,                   -- READ_DATA_WIDTH_B-bit output: Data output for port B read operations.
-        --  sbiterrb => sbiterrb,             -- 1-bit output: Status signal to indicate single bit error occurrence
-                                           -- on the data output of port B.
+    --      doutb => mem_out_b,--doutb,                   -- READ_DATA_WIDTH_B-bit output: Data output for port B read operations.
+    --     --  sbiterrb => sbiterrb,             -- 1-bit output: Status signal to indicate single bit error occurrence
+    --                                        -- on the data output of port B.
    
-         addra => mem_addr_a,                   -- ADDR_WIDTH_A-bit input: Address for port A write operations.
-         addrb => mem_addr_b,                   -- ADDR_WIDTH_B-bit input: Address for port B read operations.
-         clka => clk,                     -- 1-bit input: Clock signal for port A. Also clocks port B when
-                                           -- parameter CLOCKING_MODE is "common_clock".
+    --      addra => mem_addr_a,                   -- ADDR_WIDTH_A-bit input: Address for port A write operations.
+    --      addrb => mem_addr_b,                   -- ADDR_WIDTH_B-bit input: Address for port B read operations.
+    --      clka => clk,                     -- 1-bit input: Clock signal for port A. Also clocks port B when
+    --                                        -- parameter CLOCKING_MODE is "common_clock".
    
-         clkb => clk,                     -- 1-bit input: Clock signal for port B when parameter CLOCKING_MODE is
-                                           -- "independent_clock". Unused when parameter CLOCKING_MODE is
-                                           -- "common_clock".
+    --      clkb => clk,                     -- 1-bit input: Clock signal for port B when parameter CLOCKING_MODE is
+    --                                        -- "independent_clock". Unused when parameter CLOCKING_MODE is
+    --                                        -- "common_clock".
    
-         dina => mem_in_a,--dina,                     -- WRITE_DATA_WIDTH_A-bit input: Data input for port A write operations.
-         ena => ena,                       -- 1-bit input: Memory enable signal for port A. Must be high on clock
-                                           -- cycles when write operations are initiated. Pipelined internally.
+    --      dina => mem_in_a,--dina,                     -- WRITE_DATA_WIDTH_A-bit input: Data input for port A write operations.
+    --      ena => ena,                       -- 1-bit input: Memory enable signal for port A. Must be high on clock
+    --                                        -- cycles when write operations are initiated. Pipelined internally.
    
-         enb => ena,                       -- 1-bit input: Memory enable signal for port B. Must be high on clock
-                                           -- cycles when read operations are initiated. Pipelined internally.
+    --      enb => ena,                       -- 1-bit input: Memory enable signal for port B. Must be high on clock
+    --                                        -- cycles when read operations are initiated. Pipelined internally.
    
-         injectdbiterra => '0',--injectdbiterra, -- 1-bit input: Controls double bit error injection on input data when
-                                           -- ECC enabled (Error injection capability is not available in
-                                           -- "decode_only" mode).
+    --      injectdbiterra => '0',--injectdbiterra, -- 1-bit input: Controls double bit error injection on input data when
+    --                                        -- ECC enabled (Error injection capability is not available in
+    --                                        -- "decode_only" mode).
    
-         injectsbiterra => '0',--injectsbiterra, -- 1-bit input: Controls single bit error injection on input data when
-                                           -- ECC enabled (Error injection capability is not available in
-                                           -- "decode_only" mode).
+    --      injectsbiterra => '0',--injectsbiterra, -- 1-bit input: Controls single bit error injection on input data when
+    --                                        -- ECC enabled (Error injection capability is not available in
+    --                                        -- "decode_only" mode).
    
-         regceb => '1',--regceb,                 -- 1-bit input: Clock Enable for the last register stage on the output
-                                           -- data path.
+    --      regceb => '1',--regceb,                 -- 1-bit input: Clock Enable for the last register stage on the output
+    --                                        -- data path.
    
-         rstb => rst,                     -- 1-bit input: Reset signal for the final port B output register
-                                           -- stage. Synchronously resets output port doutb to the value specified
-                                           -- by parameter READ_RESET_VALUE_B.
+    --      rstb => rst,                     -- 1-bit input: Reset signal for the final port B output register
+    --                                        -- stage. Synchronously resets output port doutb to the value specified
+    --                                        -- by parameter READ_RESET_VALUE_B.
    
-         sleep => '0',--sleep,                   -- 1-bit input: sleep signal to enable the dynamic power saving feature.
-         wea => b"1" --wea                        -- WRITE_DATA_WIDTH_A/BYTE_WRITE_WIDTH_A-bit input: Write enable vector
-                                           -- for port A input data port dina. 1 bit wide when word-wide writes
-                                           -- are used. In byte-wide write configurations, each bit controls the
-                                           -- writing one byte of dina to address addra. For example, to
-                                           -- synchronously write only bits [15-8] of dina when WRITE_DATA_WIDTH_A
-                                           -- is 32, wea would be 4'b0010.
+    --      sleep => '0',--sleep,                   -- 1-bit input: sleep signal to enable the dynamic power saving feature.
+    --      wea => b"1" --wea                        -- WRITE_DATA_WIDTH_A/BYTE_WRITE_WIDTH_A-bit input: Write enable vector
+    --                                        -- for port A input data port dina. 1 bit wide when word-wide writes
+    --                                        -- are used. In byte-wide write configurations, each bit controls the
+    --                                        -- writing one byte of dina to address addra. For example, to
+    --                                        -- synchronously write only bits [15-8] of dina when WRITE_DATA_WIDTH_A
+    --                                        -- is 32, wea would be 4'b0010.
    
-      );
-      o_dout_b   <= mem_out_b(MEM_WIDTH -1 downto 1);
-      o_dv_out_b <= mem_out_b(0);
-    end generate XPM;
+    --   );
+    --   o_dout_b   <= mem_out_b(MEM_WIDTH -1 downto 1);
+    --   o_dv_out_b <= mem_out_b(0);
+    -- end generate XPM;
     
   
   
